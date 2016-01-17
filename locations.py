ee43271c2bf8de2620336573afb584fad9d4e602
davochka <NhAiGmkaTjnXJEjIwr0P>
2016-01-17 12:23:46 +0700

    wrapped

--- locations.py
+++ locations.py
@@ -3,48 +3,115 @@
 # 2016 dava
 # locations based on https://github.com/rimar/wifi-location-changer
 
+import json
 import time
 import subprocess
+import logging
 from subprocess import Popen, PIPE
 
-TELEZON_HOSTS = ("TelezonOffice", "office")
-#MOUNT_LIST =
 
-def run_command(cmd):
-    return subprocess.Popen(cmd,
-                            shell=True,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            stdin=subprocess.PIPE).communicate()[0].split('\n')
+config_file = "settings.json"
+
+class Location:
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.settings = self.load_config()
+
+    def load_config(self):
+        """
+        Simply returns loaded json config
+        """
+        try:
+            json_data = open(self.config_file)
+            data = json.load(json_data)
+        except Exception, e:
+            #logging.critical('Config file has errors, aborting – %s' % e)
+            raise e
+        else:
+            pass
+            #logging.info('Config – %s' % config_file)
+        json_data.close()
+        return data
+
+    def check_locations(self, location):
+        """
+        Returns True if location defined in settings.json
+        """
+        if location in self.settings['Locations']:
+            return True
+        else:
+            return False
+
+    def check_mount_point(self, location):
+        if len(self.settings['Points'][location]) > 0:
+            return True
+        else:
+            return False
+
+    def need_switch_location(self, SSID, current_location):
+        location = 'none'
+        for key, value in self.settings['Locations'].items():
+            for v in value:
+                #print(v)
+                if SSID in v:
+                    location = key
+        if location != 'none':
+            if location != current_location:
+                return True, location
+            else:
+                return False, location
+        else:
+            return False, location
+
+    def switch_location(self, location):
+        cmd = 'scselect %s' % location
+        self.run_command(cmd)
+
+    def automount(self, location):
+        for line in self.settings['Points'][location]:
+            tell = ('set sfiles to "'+ line +
+                    '"\ntell application "Finder"'
+                    '\nmount volume sfiles'
+                    '\nend tell')
+            cmd = 'osascript -e \'' + tell + '\''
+            self.run_command(cmd)
+
+    def get_ssid(self):
+        SSID = self.run_command('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I'
+            '| grep \' SSID:\' | cut -d \':\' -f 2 | tr -d \' \'')[0]
+        return SSID
+
+    def get_locations(self):
+        location_list = self.run_command('scselect | tail -n +2 | cut -d \"(\" -f 2 | cut -d \")\" -f 1')
+        return location_list
+
+    def get_current_location(self):
+        current_location = self.run_command('scselect | grep \" \* \" | cut -d \"(\" -f 2 | cut -d \")\" -f 1')[0]
+        return current_location
+
+    def run_command(self, cmd):
+        return subprocess.Popen(cmd,
+                                shell=True,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                stdin=subprocess.PIPE).communicate()[0].split('\n')
+
 
 time.sleep(2)
 
-SSID = run_command('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I' \
-        '| grep \' SSID:\' | cut -d \':\' -f 2 | tr -d \' \'')[0]
-LOCATION_NAMES=run_command('scselect | tail -n +2 | cut -d \"(\" -f 2 | cut -d \")\" -f 1')
-CURRENT_LOCATION=run_command('scselect | grep \" \* \" | cut -d \"(\" -f 2 | cut -d \")\" -f 1')[0]
+loc = Location(config_file)
+SSID = loc.get_ssid()
+locations_list = loc.get_locations()
+current_location = loc.get_current_location()
 
-NEW_LOCATION = 'unknown'
+if not loc.check_locations(current_location):
+    raise SystemExit(0)
 
-if SSID in TELEZON_HOSTS:
-    SSID = 'Telezon'
+if not loc.check_mount_point(current_location):
+    raise SystemExit(0)
 
-if SSID in LOCATION_NAMES:
-    NEW_LOCATION = SSID
+need_switch, new_location = loc.need_switch_location(SSID, current_location)
 
-else:
-    if 'Automatic' in LOCATION_NAMES:
-        NEW_LOCATION = 'Automatic'
-    elif 'auto' in LOCATION_NAMES:
-        NEW_LOCATION = 'auto'
-    else:
-        print "Automatic location was not found!"
-        print "The following locations are known:"
-        print LOCATION_NAMES
-
-if NEW_LOCATION != CURRENT_LOCATION:
-        print "Changing to %s" % NEW_LOCATION
-        cmd = 'scselect %s' % NEW_LOCATION
-        run_command(cmd)
-        cmd = 'osascript -e \'mount volume "smb://ddu@192.168.3.42/data"\''
-        run_command(cmd)
+if need_switch:
+    loc.switch_location(new_location)
+    loc.automount(new_location)
